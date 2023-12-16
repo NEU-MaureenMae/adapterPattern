6f17bf6a68e7a8f9defff3564a99893412af4bfa
stud-mauu <maureen.sebastian@neu.edu.ph>
2023-12-17 00:36:29 +0800

    Implemented RefrigeratorAdapter adapting Refrigerator to PowerOutlet interface

---
+++ src/src/RefrigeratorAdapter.java
@@ -0,0 +1,12 @@
+class RefrigeratorAdapter implements PowerOutlet {
+    private final Refrigerator refrigerator;
+
+    public RefrigeratorAdapter(Refrigerator refrigerator) {
+        this.refrigerator = refrigerator;
+    }
+
+    @Override
+    public void plugIn() {
+        refrigerator.startCooling();
+    }
+}
